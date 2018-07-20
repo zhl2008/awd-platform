34a035b35631fc41a29eb5318eaba3d06a362644
zhl2008 <zhl2008vvvss@gmail.com>
2018-07-20 17:52:50 +0800

    fix some bugs in gen_flag.py

--- check_server/docker.sh
+++ check_server/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-docker run -v `pwd`:/var/www/html   -d --name check_server -ti web_14.04:latest 
+docker run -v `pwd`:/var/www/html -v `pwd`/../flag_server:/var/www/html/flag_server   -d --name check_server -ti web_14.04:latest /var/www/html/run.sh 
--- check_server/gen_flag.py
+++ check_server/gen_flag.py
@@ -2,14 +2,15 @@
 
 import requests
 import hashlib
+import time
 
 port = 9999
 time_span = 2 * 60
-record_path = '../flag_server/xxxxxxxx_flag'
+record_path = './flag_server/xxxxxxxx_flag'
 
 
 hosts = open('host.lists').readlines()
-passwds = open('../pass.txt').readlines()
+passwds = open('./pass.txt').readlines()
 
 
 password_list = {}
@@ -27,13 +28,14 @@ for host in hosts:
 while True:
 	open(record_path,'w').write('')
 	for teamno in host_list:
-		flag = hashlib.md5().hexdigest(password_list[teamno] +  host_list[teamno] +  str(int(time.time())/time_span))
+		flag = hashlib.md5(password_list[teamno] +  host_list[teamno] +  str(int(time.time())/time_span)).hexdigest()
 
-		url_1 = 'http://' + host + ':' + port + '/' + password_list[teamno] + '/' + flag
+		url_1 = 'http://' + host_list[teamno] + ':' + str(port) + '/' + password_list[teamno] + '/' + flag
 		try:
 			r = requests.get(url_1)
 		except Exception,e:
 			print '[!] error:' + str(e)
-		open(record_path,'a').write(host_list[teamno] + ':' + password_list[teamno] + "\n" )
-		print '[*] flag for %s update to %s!' % (host_list[teamno],password_list[teamno])
+		open(record_path,'a').write(host_list[teamno] + ':' + flag + "\n" )
+		print '[*] flag for %s update to %s!' % (host_list[teamno],flag)
 
+	time.sleep(time_span)
